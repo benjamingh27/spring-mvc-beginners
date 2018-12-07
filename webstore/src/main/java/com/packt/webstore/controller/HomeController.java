4c770952b779d71d1169644fe67b58b8e8d4f2e7
benjamingh27 <bgranadosh@gmail.com>
2018-12-06 19:44:36 -0600

    se hace commit

--- webstore/src/main/java/com/packt/webstore/controller/HomeController.java
+++ webstore/src/main/java/com/packt/webstore/controller/HomeController.java
@@ -2,17 +2,32 @@ package com.packt.webstore.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
 	@RequestMapping
-	public String welcome(Model model) {
+	public String welcome(Model model, RedirectAttributes redirectAttributes) {
 		
 		model.addAttribute("greeting", "Welcome to Web Store!");
 		model.addAttribute("tagline", "The one and only amazing web store!");
+		
+		redirectAttributes.addFlashAttribute("greeting", "Welcome to Web Store!");
+		redirectAttributes.addFlashAttribute("tagline", "The one and only amazing web store!");
+		
+		return "redirect:/welcome/greeting";
+	}
+	
+	@RequestMapping("/welcome/greeting")
+	public String greeting(@ModelAttribute("greeting") String greeting, @ModelAttribute("tagline") String tagline, Model model) {
+		
+		model.addAttribute("greeting", greeting);
+		model.addAttribute("tagline", tagline);
 		
 		return "welcome";
 	}
