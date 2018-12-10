2ff8009722898b9344447681742ad022a4557047
benjamingh27 <bgranadosh@gmail.com>
2018-12-10 17:09:22 -0600

    Se hace commit del archivo de configuracion

--- webstore/src/main/java/com/packt/webstore/config/WebApplicationContextConfig.java
+++ webstore/src/main/java/com/packt/webstore/config/WebApplicationContextConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -48,6 +49,12 @@ public class WebApplicationContextConfig extends WebMvcConfigurerAdapter {
 		resource.setBasename("messages");
 		
 		return resource;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/img/**").
+				 addResourceLocations("/resources/images/");
 	}
 	
 }
